b4efcf4dcaa71c7d491fbf2bdbe1889c08f199c3
Guy Raz Nir <guynir75@gmail.com>
2024-02-14 08:58:34 +0200

    Create production-environment Docker compose descriptor
    - Includes all services - database, webapp-ui, webapp-boot (backend)
    - Runs database migration
    - Incorporates HTTP router.

--- sys/docker/compose/stop.bat
+++ sys/docker/compose/stop.bat
@@ -7,4 +7,6 @@ ECHO *                                                         *
 ECHO ***********************************************************
 ECHO.
 
+SET PROJECT_NAME=fuzzylist
+
 docker-compose -p %PROJECT_NAME% -f docker-compose-windows-mac.yaml down
